13f8653fecf5447e8405b25ca8c372ed1e876a37
Victor Albertos Gil <me@victoralbertos.com>
2020-08-05 22:33:46 +0200

    add 2.9

---
+++ src/apuntes/2.9 Variables, constantes y palabras reservadas.kt
@@ -0,0 +1,20 @@
+package apuntes
+
+
+fun main() {
+    var age = 4
+    print(4)
+
+    age = 5
+    print(5)
+
+    var name = "laura"
+    print(name)
+
+    name = "joana"
+    print(name)
+
+    val kilos = 5
+    print(kilos)
+    //kilos = 4 no compilará porque val es para valores inmutables
+}
