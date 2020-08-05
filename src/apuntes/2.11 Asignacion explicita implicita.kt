c83901fadf6acfe2c9d0984f024b6f6b06922c48
Victor Albertos Gil <me@victoralbertos.com>
2020-08-05 22:38:27 +0200

    2.11 Asignacion explicita implicita

---
+++ src/apuntes/2.11 Asignacion explicita implicita.kt
@@ -0,0 +1,11 @@
+package apuntes
+
+fun main() {
+    val name = "laura"
+    val nameExplicito : String = "laura"
+    val age = 34
+    val ageExplicito : Int = 34
+
+    //val incoherencia : String = 100
+    //no compila porque el tipo de la variable se ha declarado como String pero se le esta asignando un valor numerico
+}
